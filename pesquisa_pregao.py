041f13cb91993097b0874a47e3125cdaa85272a7
Guilherme Holanda Sanches <38058605+holondo@users.noreply.github.com>
2022-02-22 16:41:54 -0300

    Class constructor accepting list or dict

--- pesquisa_pregao.py
+++ pesquisa_pregao.py
@@ -9,39 +9,40 @@ from zipfile import ZipFile
 
 from loguru import logger
 
-
+from utils.clearing_files_scraper import get_latest_report_updates
 class PesquisaPregao():
     """
         Downloads clearing files from B3(São Paulo's stock exchange)
         Params:
-            * report_date_dict: Dictionary containing:
+            * reports: dict or list containing:
                 * (str) report name as key
-                * (datetime or str) date in the format "dd/mm/yyyy" as value
+                * (datetime or str) date in the format "dd/mm/yyyy" as value if reports is a dict
     """
-    def __init__(self, report_date_dict:'dict[str, Union[str, datetime]]'):
+    def __init__(self, reports:Union['list[str]', 'dict[str, Union[str, datetime]]']):
         clearing_files_path = os.path.join(os.path.dirname(__file__), './data/clearing_file.json')
         with open(clearing_files_path) as file:
             self.__CLEARING_FILENAMES__:'dict[str, str]' = json.load(file)
             
-        
-        inconsistent = []
-        for report_name in report_date_dict:
+        reports_latest_updates = get_latest_report_updates()
+        reports_date_dict = {}
+
+        for report_name in reports:
             if report_name.lower() not in self.__CLEARING_FILENAMES__.keys():
                 logger.warning(f'Report {report_name} does not exists, ignoring.')
-                inconsistent.append(report_name)
+                continue
+            
+            if type(reports) == dict:
+                if type(reports[report_name]) != datetime:
+                    try:
+                        reports_date_dict[report_name] = datetime.strptime(reports[report_name], '%d/%m/%Y')
+                    except ValueError as e:
+                        logger.warning(f'Date format exception for report {report_name}, ignoring it.')
+                        logger.warning(str(e))
 
-            elif type(report_date_dict[report_name]) != datetime:
-                try:
-                    report_date_dict[report_name] = datetime.strptime(report_date_dict[report_name], '%d/%m/%Y')
-                except ValueError as e:
-                    logger.warning(f'Date format exception for report {report_name}, ignoring it.')
-                    logger.warning(str(e))
-                    inconsistent.append(report_name)
+            elif type(reports) == list:
+                reports_date_dict[report_name.lower()] = reports_latest_updates[report_name.lower()]
 
-        for inconsistent_report in inconsistent:
-            del report_date_dict[inconsistent_report] # Deleting inconsistent parameters
-
-        self.selected_reports = report_date_dict
+        self.selected_reports = reports_date_dict
 
     def download(self, directory_path:str):
         """
@@ -69,5 +70,6 @@ class PesquisaPregao():
 if __name__ == '__main__':
     # test
     # a = PesquisaPregao({"mercado de derivativos - operacoes estruturadas de volatilidade" : '12/01/2022', 'Mapeamento de Grupos de Instrumentos Padronizados' : '12/01/2022', 'aab' : 'baa'})
-    # a.download() #Add file path
+    a = PesquisaPregao(["mercado de derivativos - operacoes estruturadas de volatilidade", 'Mapeamento de Grupos de Instrumentos Padronizados'])
+    a.download() #Add file path
     pass
